ca0c6d16a136f4e2d198452c6f71174a641e2dd5
araso2000 <a.raso.pasero@gmail.com>
2022-02-03 11:51:54 +0100

    problems

--- src/main/java/com/nftforme/urjc/NftForMe1Application.java
+++ src/main/java/com/nftforme/urjc/NftForMe1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NftForMeApplication {
+public class NftForMe1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NftForMeApplication.class, args);
+		SpringApplication.run(NftForMe1Application.class, args);
 	}
 
 }
--- src/test/java/com/nftforme/urjc/NftForMe1ApplicationTests.java
+++ src/test/java/com/nftforme/urjc/NftForMe1ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NftForMeApplicationTests {
+class NftForMe1ApplicationTests {
 
 	@Test
 	void contextLoads() {
