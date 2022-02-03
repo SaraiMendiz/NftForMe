614daaf2814ac816a1ebf27931501def24c52d02
araso2000 <a.raso.pasero@gmail.com>
2022-02-03 11:43:13 +0100

    Starter Project

---
+++ src/main/java/com/nftforme/urjc/NftForMeApplication.java
@@ -0,0 +1,13 @@
+package com.nftforme.urjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftForMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftForMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nftforme/urjc/NftForMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nftforme.urjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftForMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
