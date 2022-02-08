64c270f9e23eede787763ac1bea9e1d99d143b8c
sstsanta <sstsanta@gmail.com>
2022-02-08 13:55:14 +0100

    aa

--- src/main/java/cliente/Cliente.java
+++ src/main/java/cliente/Cliente.java
@@ -1,6 +1,14 @@
 package cliente;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private String nombre;
 	private int ID;
 	private float saldo;
